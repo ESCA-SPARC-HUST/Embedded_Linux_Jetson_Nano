1f5ce07381c24bae788af757759f5c9c0cc2dcac
Minkeisme <minhnguyencao1204@gmail.com>
2023-11-27 19:54:07 +0700

    recover deleted code

---
+++ record-module/README.md
@@ -0,0 +1,10 @@
+Flow of start_setting.txt file 
+//file location 
+//framerate 
+//samplewidth
+//sample format 
+//number of channels 
+//time duration per file 
+
+Syntax when calling "client" program:
+    ./clients -<command> <content>
--- record-module/server.c
+++ record-module/server.c
@@ -6,10 +6,11 @@
 #include <string.h>
 #include <sys/socket.h>
 #include "record.h"
-
+#include "command.h"
 
 //define for socket
 #define PORT 8080
+#define BUFFER_SIZE 1024
 
 // define for collecting data
 #define FRAME_TO_CAPTURE 1024
@@ -18,25 +19,96 @@ int SAMPLE_RATE = 44100;
 int CHANNELS = 2;
 int BITS_PER_SAMPLE  = 16;
 int SAMPLE_FORMAT = SND_PCM_FORMAT_S16_LE; 
-
+int TIME_RECORD = 5; 
+char RECORD_LOCATION[BUFFER_SIZE/4];
 
 // control recoding status
 int* recording;
 int status = 0;
 
-
-
 //define for multi-threading
 pthread_t tid1, tid2;
 pthread_attr_t attr;
 
+void *main_socket (void* data);
+void *recording_handler (void* data);
+void setup();
+void update_settings(int n, char replace_content[]);
+
+// void debug() {
+//     printf("Location: %s\n", RECORD_LOCATION);
+//     printf("Sample rate: %d\n", SAMPLE_RATE);
+//     printf("Sample width: %d\n", BITS_PER_SAMPLE);
+//     printf("Sample format: %d\n", SAMPLE_FORMAT);
+//     printf("Num of channels: %d\n", CHANNELS);
+//     printf("Time per audio file: %d\n", TIME_RECORD);
+// }
+
+int main(int argc, char * argv[]) {
+    setup();
+    void* status;
+    if(pthread_create(&tid1, NULL, main_socket, NULL)) {
+        perror("Failure!");
+        exit(1);
+    }
+    pthread_join(tid1, NULL);
+}
+
+void setup() {
+    FILE* file = fopen("start_setting.txt", "r");
+    char content[BUFFER_SIZE/4];
+    fseek(file, 0, SEEK_SET);
+    
+    fgets(content, sizeof(content), file);
+    content[strlen(content)-1] = '\0';
+    strcpy(RECORD_LOCATION, content);
+
+    fgets(content, sizeof(content), file);
+    SAMPLE_RATE = atoi(content);
+
+    fgets(content, sizeof(content), file);
+    BITS_PER_SAMPLE = atoi(content);
+
+    fgets(content, sizeof(content), file);
+    SAMPLE_FORMAT = atoi(content);
+
+    fgets(content, sizeof(content), file);
+    CHANNELS = atoi(content);
+    
+    fgets(content, sizeof(content), file);
+    TIME_RECORD = atoi(content);
+
+    printf("Location: %s\n", RECORD_LOCATION);
+    printf("Sample rate: %d\n", SAMPLE_RATE);
+    printf("Sample width: %d\n", BITS_PER_SAMPLE);
+    printf("Sample format: %d\n", SAMPLE_FORMAT);
+    printf("Num of channels: %d\n", CHANNELS);
+    printf("Time per audio file: %d\n", TIME_RECORD);
+
+    fclose(file);
+}
+
+void update_settings(int n, char replace_content[]) {
+    FILE *file = fopen("start_setting.txt", "r");
+    char content[16][128];
+    int no_line = 1; 
+    while (fgets(content[no_line], sizeof(content[no_line]), file)) {
+        no_line++; 
+    }
+    fclose(file);
+    strcpy(content[n], replace_content);
+    file = fopen("start_setting.txt", "w");
+    for(int i = 1; i < no_line; i++) {
+        fputs(content[i], file);
+    }
+    fclose(file);
+}
 
 void *recording_handler (void* data) {
     initialize(SND_PCM_STREAM_CAPTURE, SAMPLE_FORMAT, SAMPLE_RATE, CHANNELS);
-    implement (FRAME_TO_CAPTURE, SAMPLE_FORMAT, CHANNELS, SAMPLE_RATE, BITS_PER_SAMPLE, recording);
+    implement (FRAME_TO_CAPTURE, SAMPLE_FORMAT, CHANNELS, SAMPLE_RATE, BITS_PER_SAMPLE, recording, RECORD_LOCATION);
     snd_pcm_close(handle);
 }
-
 
 void* main_socket (void* data) {
     int server_fd, new_socket;
@@ -45,7 +117,7 @@ void* main_socket (void* data) {
     int opt = 1;
     recording = &status;
     socklen_t addrlen = sizeof(address);
-    char buffer[8] = { 0 };
+    char buffer[BUFFER_SIZE] = { 0 };
  
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -65,9 +137,7 @@ void* main_socket (void* data) {
     address.sin_port = htons(PORT);
  
     // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
@@ -77,46 +147,72 @@ void* main_socket (void* data) {
     }
 
     while(1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        valread = read(new_socket, buffer, BUFFER_SIZE - 1); // subtract 1 for the null
+                                // terminator at the end
+        printf("The buffer: %s\n", buffer);
 
-    if ((new_socket
-         = accept(server_fd, (struct sockaddr*)&address,
-                  &addrlen))
-        < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read(new_socket, buffer,
-                   8 - 1); // subtract 1 for the null
-                              // terminator at the end
-    printf("The buffer: %s\n", buffer);
-    if(strcmp(buffer, "start") == 0) {
-        status = 1;
-        if(pthread_create(&tid2, NULL, recording_handler, NULL)) {
-        perror("Failure!");
-        exit(2);
-    }
-    }
-    if(strcmp(buffer, "end") == 0) {
-        status = 0;
-    }
-    for(int i = 0; i < 7; ++i)
-        buffer[i] = NULL;
-    // sleep(5);
-    // closing the connected socket
-    close(new_socket);
+        char content[BUFFER_SIZE/4];
+        char command;
+        sscanf(buffer, "-%c:%s", &command, content);
+        printf("Command: %c\n", command);
+        printf("Content: %s\n", content);
+
+        if(command == START) {
+            status = 1;
+            if(pthread_create(&tid2, NULL, recording_handler, NULL)) {
+                perror("Failure!");
+                exit(2);
+            }
+        }
+        else if(command == END) {
+            status = 0;
+        }
+        else if(command == LOCATION) {
+            strcpy(RECORD_LOCATION, content); 
+            printf("Location: %s\n", RECORD_LOCATION);
+            strcat(content, "\n");
+            update_settings(1, content);
+            // debug();
+        }
+        else if(command == NCHANNELS) {
+            CHANNELS = atoi(content);
+            strcat(content, "\n");
+            update_settings(5, content);
+            // debug();
+        }
+        else if(command == FRAME_RATE) {
+            SAMPLE_RATE = atoi(content);
+            strcat(content, "\n");
+            update_settings(2, content);
+            // debug();
+        } 
+        else if(command == SAMPLE_WIDTH) {
+            BITS_PER_SAMPLE = atoi(content);
+            strcat(content, "\n");
+            update_settings(3, content);
+            // debug();
+        }
+        else if(command == SFORMAT) {
+            SAMPLE_FORMAT = atoi(content);
+            strcat(content, "\n");
+            update_settings(4, content);
+            // debug();
+        }
+        else if(command == RECORD_DURATION) {
+            TIME_RECORD = atoi(content);
+            strcat(content, "\n");
+            update_settings(5, content);
+            // debug();
+        }
+
+        for(int i = 0; i < BUFFER_SIZE; ++i) buffer[i] = 0;
+        // sleep(5);
+        // closing the connected socket
+        close(new_socket);
     }
     return 0;
 }
-
-
-
-int main(int argc, char * argv[])
-{
-    void* status;
-    if(pthread_create(&tid1, NULL, main_socket, NULL)) {
-        perror("Failure!");
-        exit(1);
-    }
-    pthread_join(tid1, NULL);
-}
-
