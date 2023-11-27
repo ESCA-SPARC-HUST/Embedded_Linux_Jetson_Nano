e73ee2c75eb27a9640307992e42f76f1d25c23ac
Minkeisme <minhnguyencao1204@gmail.com>
2023-11-27 15:27:15 +0700

    Add customize file location and optimize command sending

---
+++ record-module/command.h
@@ -0,0 +1,12 @@
+#define RECORD_DURATION 't' // command syntax:   -RCD value
+#define START_TIME 's'// command syntax:   -ST hh:mm:ss
+#define END_TIME 'e'// command syntax:   -ET hh:mm:ss
+#define FRAME_RATE 'F'// command syntax:   -FR value 
+#define SAMPLE_WIDTH 'B'// command syntax:   -SW value
+#define NCHANNELS 'C' // command syntax: -CNL value 
+#define MODE 'm' // command syntax: -m value
+#define START 'S' // command syntax: -
+#define END 'E' // command syntax: -
+#define LOCATION 'L'
+// #define CANCEL_SCHEDULE "CSH"// command syntax:   -CSH
+
